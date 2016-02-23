9490c256a4c39c70ff9a8acd247e4744ae3f8aa9
JaeRyong Seo <ranian1974@gmail.com>
2016-02-23 17:16:51 +0900

    world is mine

--- ConsoleApplication2/Program.cs
+++ ConsoleApplication2/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApplication2
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
+            Console.WriteLine("World is mine");
         }
     }
 }
