7469e899c667a97517a87b3a6647fb1308ca2838
JaeRyong Seo <ranian1974@gmail.com>
2016-02-24 10:07:39 +0900

    add CTestPos and README.md

---
+++ ConsoleApplication2/CTestPos.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class CTestPos
+    {
+        double X;
+        double Y;
+        double T;
+    }
+}
